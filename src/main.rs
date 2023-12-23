8c82a3bf353f2c3a5e88d5e1960f1f51a410e34e
ouharetaso <orangepi.hazimekara@gmail.com>
2023-12-23 14:01:23 +0900

    impl ops to vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod vector;
+use vector::Vector;
+
+
 fn main() {
-    println!("Hello, world!");
+    let a : Vector<f32, 3> = Vector{ v : [1., 1., 4.] };
+    let b : Vector<f32, 3> = Vector{ v : [5., 1., 4.] };
+    
+    println!("{} + {} = {}", a, b, a + b);
+    println!("{} * {} = {}", 4, b, 4 * b);
+    
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,11 +1,16 @@
 use std::ops::*;
+use std::fmt;
+use num_traits::real::Real;
+use num_traits::cast::NumCast;
 
-pub struct Vector <T, const N: usize> {
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vector <T: Real, const N: usize> {
     pub v : [T; N],    
 }
 
 
-impl<T, const N: usize> Default for Vector <T, N>
+impl<T: Real, const N: usize> Default for Vector <T, N>
 where
     T: Default + Copy,
 {
@@ -17,7 +22,165 @@ where
 }
 
 
-impl<T, const N: usize> Index<usize> for Vector <T, N> {
+impl<T: Real, const N: usize> std::fmt::Display for Vector <T, N>
+where
+    T: std::fmt::Display
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut formatted = String::from("[");
+        for i in 0..N {
+            formatted.push_str(&self[i].to_string());
+            if i < N-1 {
+                formatted.push_str(", ");
+            }
+            else{
+                ()
+            }
+        }
+        formatted.push_str("]");
+        write!(f, "{}", formatted)
+    }
+}
+
+
+/*
+    Operators for Vector
+
+    Neg
+    Add
+    AddAssign
+    Sub
+    SubAssign
+    Mul
+    Index
+    IndexMut
+*/
+
+impl <T: Real, const N: usize> Neg for Vector <T, N>
+where 
+    T: std::ops::Neg<Output = T> + Default
+{
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        let mut ret: Vector<T, N> = Default::default();
+        for i in 0..N {
+            ret[i] = -self[i];
+        }
+
+        return ret;
+    }
+}
+
+
+impl <T: Real, const N: usize> Add for Vector <T, N>
+where 
+    T: std::ops::Add<Output = T> + Default
+{
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        let mut ret: Vector<T, N> = Default::default();
+        for i in 0..N {
+            ret[i] = self[i] + other[i];
+        }
+
+        return ret;
+    }
+}
+
+
+impl <T: Real, const N: usize> AddAssign for Vector <T, N>
+where 
+    T: std::ops::AddAssign
+{
+    fn add_assign(&mut self, other: Self) {
+        for i in 0..N {
+            self[i] += other[i]
+        }
+    }
+}
+
+
+impl <T: Real, const N: usize> Sub for Vector <T, N>
+where 
+    T: std::ops::Sub<Output = T> + Default
+{
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let mut ret: Vector<T, N> = Default::default();
+        for i in 0..N {
+            ret[i] = self[i] - other[i];
+        }
+
+        return ret;
+    }
+}
+
+
+impl <T: Real, const N: usize> SubAssign for Vector <T, N>
+where 
+    T: std::ops::SubAssign
+{
+    fn sub_assign(&mut self, other: Self) {
+        for i in 0..N {
+            self[i] -= other[i]
+        }
+    }
+}
+
+
+impl <T: Real, const N: usize, U: Real> Mul<U> for Vector <T, N>
+where 
+    T: std::ops::Mul<Output = T> + Default,
+    U: Into<T>
+{
+    type Output = Self;
+
+    fn mul(self, other: U) -> Self::Output {
+        let mut ret: Vector<T, N> = Default::default();
+        for i in 0..N {
+            ret[i] = self[i] * Into::<T>::into(other);
+        }
+
+        return ret;
+    }
+}
+
+
+macro_rules! defmulvec {
+    ($($t: ty), *) => {
+        $(
+            impl<T: Real, const N: usize> Mul<Vector<T, N>> for $t
+            where 
+                T: std::ops::Mul<Output = T> + Default + NumCast
+            {   
+                type Output = Vector<T, N>;
+                fn mul(self, other: Vector<T, N>) -> Self::Output{
+                    let mut ret: Vector<T, N> = Default::default();
+                    for i in 0..N {
+                        let converted: Option<T> = NumCast::from(self);
+                        match converted {
+                            Some(value) => {
+                                ret[i] = value * other[i];
+                            }
+                            None => (),
+                        }
+                    }
+            
+                    return ret;
+                }
+            }
+        )*
+    };
+}
+
+
+defmulvec![f32, f64, i8, i16, i32, i64, u8, u16, u32, u64];
+
+
+impl<T: Real, const N: usize> Index<usize> for Vector <T, N> {
     type Output = T;
 
     fn index(&self, index: usize) -> &T {
@@ -26,20 +189,28 @@ impl<T, const N: usize> Index<usize> for Vector <T, N> {
 }
 
 
-impl<T, const N: usize> IndexMut<usize> for Vector <T, N> {
+impl<T: Real, const N: usize> IndexMut<usize> for Vector <T, N> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output{
         &mut self.v[index]
     }
 }
 
 
-pub trait VectorOperation<T, const N: usize>{
+/*
+    Operations that vector should have:
+
+    dot
+    cross
+    len
+*/
+pub trait VectorOperation<T: Real, const N: usize>{
     fn dot(&self, other: &Self) -> T;
     fn cross(&self, other: &Self) -> Self;
+    fn len(&self) -> T;
 }
 
 
-impl <T, const N: usize> VectorOperation<T, N> for Vector<T, N>
+impl <T: Real, const N: usize> VectorOperation<T, N> for Vector<T, N>
 where
     T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::ops::Mul<Output = T>
      + std::ops::AddAssign + Default + Copy, 
@@ -61,4 +232,12 @@ where
         return ret;
     }
 
+    fn len(&self) -> T {
+        let mut ret: T = T::default();
+        for i in 0..N {
+            ret += self[i] * self[i];
+        }
+
+        return ret.sqrt();
+    }
 }
